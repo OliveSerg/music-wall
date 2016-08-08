ce2d374db5fbfb186edd201b7f41e4c673900d12
Sergio <oliveserg@gmail.com>
2016-08-08 16:33:09 +0000

    new form added with validations

--- app/actions.rb
+++ app/actions.rb
@@ -5,5 +5,23 @@
 
 get '/tracks' do
   @tracks = Tracks.all
-  erb :'tacks/index'
+  erb :'tracks/index'
+end
+
+get '/tracks/new' do
+  @tracks = Tracks.all
+  erb :'tracks/new'
+end
+
+post '/tracks' do
+  @track = Tracks.new(
+    song_title: params[:song_title],
+    author: params[:author],
+    url: params[:url]
+  )
+  if @track.save
+    redirect '/tracks'
+  else
+    erb :'tracks/new'
+  end
 end
